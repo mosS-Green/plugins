029a9ed940116ff80bf4a3fa942556991d7b3c0e
mosS-Green <76946302+mosS-Green@users.noreply.github.com>
2025-01-14 15:59:39 +0530

    Update models.py

--- models.py
+++ models.py
@@ -6,6 +6,6 @@ GENERATION_CONFIG = {"temperature": 1, "max_output_tokens": 3096}
 MEDIA_MODEL = genai.GenerativeModel(
     model_name="gemini-2.0-flash-exp",
     generation_config=GENERATION_CONFIG,
-    system_instruction="Answer concisely and accurately, maintain a natural tone.",
+    system_instruction="Answer concisely and accurately, maintain a human tone. Listen to audios carefully, mostly they will be in hindi or english.",
     safety_settings=SAFETY_SETTINGS,
 )
