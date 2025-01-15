c23a3c7af437d47ff3197261fb8e7a03edc6e3e0
mosS-Green <76946302+mosS-Green@users.noreply.github.com>
2025-01-16 01:12:13 +0530

    Update models.py

--- models.py
+++ models.py
@@ -6,7 +6,6 @@ SAFETY_SETTINGS = [
     {"category": "HARM_CATEGORY_HATE_SPEECH", "threshold": "BLOCK_NONE"},
     {"category": "HARM_CATEGORY_SEXUALLY_EXPLICIT", "threshold": "BLOCK_NONE"},
     {"category": "HARM_CATEGORY_DANGEROUS_CONTENT", "threshold": "BLOCK_NONE"},
-    {"category": "HARM_CATEGORY_CIVIC_INTEGRITY", "threshold": "OFF"},
 ]
 
 GENERATION_CONFIG = {"temperature": 0.8, "max_output_tokens": 3096}
