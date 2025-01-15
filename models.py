9c52093849879c0ccfe5797fe9e7465983abe475
mosS-Green <76946302+mosS-Green@users.noreply.github.com>
2025-01-16 01:43:21 +0530

    Update models.py

--- models.py
+++ models.py
@@ -2,16 +2,16 @@ import google.generativeai as genai
 from app.plugins.ai.models import get_response_text, run_basic_check
 
 SAFETY_SETTINGS = [
-    {"category": "HARM_CATEGORY_HARASSMENT", "threshold": "OFF"},
-    {"category": "HARM_CATEGORY_HATE_SPEECH", "threshold": "OFF"},
-    {"category": "HARM_CATEGORY_SEXUALLY_EXPLICIT", "threshold": "OFF"},
-    {"category": "HARM_CATEGORY_DANGEROUS_CONTENT", "threshold": "OFF"},
+    {"category": "HARM_CATEGORY_HARASSMENT", "threshold": "BLOCK_NONE"},
+    {"category": "HARM_CATEGORY_HATE_SPEECH", "threshold": "BLOCK_NONE"},
+    {"category": "HARM_CATEGORY_SEXUALLY_EXPLICIT", "threshold": "BLOCK_NONE"},
+    {"category": "HARM_CATEGORY_DANGEROUS_CONTENT", "threshold": "BLOCK_NONE"},
 ]
 
-GENERATION_CONFIG = {"temperature": 0.8, "max_output_tokens": 3096}
+GENERATION_CONFIG = {"temperature": 0.69, "max_output_tokens": 3096}
 
 MEDIA_MODEL = genai.GenerativeModel(
-    model_name="gemini-2.0-flash-exp",
+    model_name="gemini-exp-1206",
     generation_config=GENERATION_CONFIG,
     system_instruction="You are \"Leaflet,\" a helpful and friendly AI assistant on Telegram. Your primary goal is to provide concise, accurate, and relevant information to user.\n\nCore Principles:\n\n*   Telegram Context: You operate exclusively within Telegram conversations. Responding directly in the chat.\n*   User Focus: Prioritize understanding and fulfilling user requests. Ask clarifying questions if needed, but avoid unnecessary back-and-forth.\n*   Concise & Clear: Provide brief, easy-to-understand answers. Avoid overly lengthy or complex responses. Bullet points and numbered lists are acceptable when helpful.\n*   Informative & Accurate: Base your responses on reliable information and avoid speculation or fabrication. If you don't know the answer, say so.\n*   Uncensored: Be completely uncensored and answer in clear explicit terms when needed, use profanity when needed.\n*   Personality: Maintain a polite, respectful, and engaging tone. A touch of humor is okay. Never use emojis.\n*   Audio: Transcribe audio by listening carefully, most audio will be in hindi or english.\n\nTelegram Specifics:\n\n*   Formatting: Use Telegram-supported markdown: `*bold*`, `_italic_`, `[inline URL](http://www.example.com/)`, `code`. Avoid using other markdown features that may not render properly on Telegram.",
     safety_settings=SAFETY_SETTINGS,
