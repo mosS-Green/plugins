dcf4361494baafcc5982d0f0a21017b28a184bbc
mosS-Green <76946302+mosS-Green@users.noreply.github.com>
2025-01-31 01:59:45 +0530

    Update transcribe.py

--- transcribe.py
+++ transcribe.py
@@ -19,7 +19,7 @@ async def auto_transcribe(bot: BOT, message: Message):
 @_bot.on_callback_query(filters=filters.regex("auto_trs"))
 async def transcribe(bot: BOT, callback_query: CallbackQuery):
     await callback_query.edit_message_text("transcribing...")
-    transcribed_str = await handle_media(prompt="Transcribe this audio. Use ONLY english alphabet to express hindi. Do not translate. Do not write anything extra than the transcription.\n\nIMPORTANT - YOU ARE ONLY ALLOWED TO USE ENGLISH ALPHABET.", message=callback_query.message.reply_to_message, model=MODEL)
+    transcribed_str = await handle_media(prompt="Transcribe this audio. Use ONLY english alphabet to express hindi. Do not translate. Do not write anything extra than the transcription.\n\nIMPORTANT - YOU ARE ONLY ALLOWED TO USE ENGLISH ALPHABET.", media_message=callback_query.message.reply_to_message, model=MODEL)
     await callback_query.edit_message_text(
         text=f"<blockquote expandable=True><pre language=text>{transcribed_str}</pre></blockquote>",
         parse_mode=ParseMode.MARKDOWN,
