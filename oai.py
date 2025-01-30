91a5761ea1afc407a333170749b4a74bab24d013
mosS-Green <76946302+mosS-Green@users.noreply.github.com>
2025-01-31 01:12:34 +0530

    Update oai.py
    ip locked

--- oai.py
+++ oai.py
@@ -6,10 +6,9 @@ import asyncio
 
 apikey = os.environ.get("FAPI_KEY")
 
-client = OpenAI(api_key = apikey, base_url = "https://fresedgpt.space/v1")
-
 @bot.add_cmd(cmd="g")
 async def gpt(bot: BOT, message: Message):
+    client = OpenAI(api_key = apikey, base_url = "https://fresedgpt.space/v1")
     prompt = message.input
 
     response = await asyncio.to_thread(client.chat.completions.create,
@@ -20,11 +19,16 @@ async def gpt(bot: BOT, message: Message):
     )
 
     response_text = f"4o: {response.choices[0].message.content}"
-    await message.reply(response_text)
+    await message.reply(
+        text=f"<blockquote expandable=True><pre language=text>{response_text}</pre></blockquote>",
+        parse_mode=ParseMode.MARKDOWN,
+    )
 
 
-@bot.add_cmd(cmd="img")
+@bot.add_cmd(cmd="i")
 async def generate_image(bot: BOT, message: Message):
+    client = OpenAI(api_key="zu-89d98ff1db79a5601658fdbc832f14e5", 
+                    base_url="https://api.zukijourney.com/v1")
     prompt = message.input
 
     response = await asyncio.to_thread(client.images.generate,
