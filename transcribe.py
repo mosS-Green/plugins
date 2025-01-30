c6050af8452bf846e3731ccb3ca41a78a20995e1
mosS-Green <76946302+mosS-Green@users.noreply.github.com>
2025-01-31 01:02:44 +0530

    Update transcribe.py

--- transcribe.py
+++ transcribe.py
@@ -2,8 +2,8 @@ from pyrogram import filters
 from pyrogram.types import CallbackQuery, InlineKeyboardButton, InlineKeyboardMarkup
 from ub_core import BOT, Message, bot
 
-from .media import handle_audio
-from .models import MEDIA_MODEL
+from app.plugins.ai.media_query import handle_media
+from app.plugins.ai.models import MODEL
 
 _bot: BOT = bot.bot
 
@@ -19,5 +19,8 @@ async def auto_transcribe(bot: BOT, message: Message):
 @_bot.on_callback_query(filters=filters.regex("auto_trs"))
 async def transcribe(bot: BOT, callback_query: CallbackQuery):
     await callback_query.edit_message_text("transcribing...")
-    transcribed_str = await handle_audio(prompt="Transcribe this audio. Use ONLY english alphabet to express hindi. Do not translate. Do not write anything extra than the transcription. Have good formatting.", message=callback_query.message.reply_to_message, model=MEDIA_MODEL)
-    await callback_query.edit_message_text(transcribed_str)
+    transcribed_str = await handle_media(prompt="Transcribe this audio. Use ONLY english alphabet to express hindi. Do not translate. Do not write anything extra than the transcription.\n\nIMPORTANT - YOU ARE ONLY ALLOWED TO USE ENGLISH ALPHABET.", message=callback_query.message.reply_to_message, model=MODEL)
+    await callback_query.edit_message_text(
+        text=f"<blockquote expandable=True><pre language=text>{transcribed_str}</pre></blockquote>",
+        parse_mode=ParseMode.MARKDOWN,
+    )
