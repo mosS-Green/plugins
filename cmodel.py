7755f2c5e3d50de056b44d851b71d3fd7aa3be20
mosS-Green <76946302+mosS-Green@users.noreply.github.com>
2025-02-08 22:24:16 +0530

    Update cmodel.py

--- cmodel.py
+++ cmodel.py
@@ -1,6 +1,7 @@
 import os
 import json
 from functools import wraps
+import copy
 
 from google.genai.types import (
     DynamicRetrievalConfig,
@@ -10,10 +11,11 @@ from google.genai.types import (
     Tool,
 )
 from pyrogram import filters
+from app.plugins.ai.models import Settings
 
-from app import BOT, Message, Config, extra_config, bot
+from app import BOT, Message, Config, bot
 
-PAST = None
+CMODEL = copy.deepcopy(Settings)
 
 @bot.add_cmd(cmd="fh")
 async def init_task(bot=bot, message=None):
@@ -23,45 +25,7 @@ async def init_task(bot=bot, message=None):
         chat_id=Config.LOG_CHAT, message_ids=past_message_id
     )
 
-    global PAST
-    PAST = json.loads(past_message.text)
+    CMODEL.CONFIG.system_instruction = json.loads(past_message.text)
 
     if message is not None:
         await message.reply("Done.", del_in=5)
-
-
-class Fast:
-    MODEL = "gemini-2.0-flash"
-
-    # fmt:off
-    CONFIG = GenerateContentConfig(
-
-        system_instruction=PAST if PAST is not None else "",
-
-        temperature=0.9,
-
-        max_output_tokens=4000,
-
-        safety_settings=[
-            SafetySetting(category="HARM_CATEGORY_HARASSMENT", threshold="BLOCK_NONE"),
-            SafetySetting(category="HARM_CATEGORY_HATE_SPEECH", threshold="BLOCK_NONE"),
-            SafetySetting(category="HARM_CATEGORY_SEXUALLY_EXPLICIT", threshold="BLOCK_NONE"),
-            SafetySetting(category="HARM_CATEGORY_DANGEROUS_CONTENT", threshold="BLOCK_NONE"),
-            SafetySetting(category="HARM_CATEGORY_CIVIC_INTEGRITY", threshold="BLOCK_NONE"),
-        ],
-        # fmt:on
-
-        tools=[
-            Tool(
-                google_search=GoogleSearchRetrieval(
-                    dynamic_retrieval_config=DynamicRetrievalConfig(
-                        dynamic_threshold=0.3
-                    )
-                )
-            )
-        ],
-    )
-
-    @staticmethod
-    def get_kwargs() -> dict:
-        return {"model": Fast.MODEL, "config": Fast.CONFIG}
