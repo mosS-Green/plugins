b8364bec90e95058914e57fb4cf3586e2d211136
mosS-Green <76946302+mosS-Green@users.noreply.github.com>
2025-02-08 17:26:16 +0530

    Update cmodel.py

--- cmodel.py
+++ cmodel.py
@@ -13,17 +13,19 @@ from pyrogram import filters
 
 from app import BOT, Message, Config, extra_config, bot
 
+PAST = None
+
 @bot.add_cmd(cmd="fh")
 async def init_task(bot=bot, message=None):
     past_message_id = int(os.environ.get("PAST_MESSAGE_ID"))
-    
+
     past_message = await bot.get_messages(
         chat_id=Config.LOG_CHAT, message_ids=past_message_id
     )
 
     global PAST
     PAST = json.loads(past_message.text)
-    
+
     if message is not None:
         await message.reply("Done.", del_in=5)
 
@@ -34,7 +36,7 @@ class Fast:
     # fmt:off
     CONFIG = GenerateContentConfig(
 
-        system_instruction=PAST,
+        system_instruction=PAST if PAST is not None else "",
 
         temperature=0.9,
 
