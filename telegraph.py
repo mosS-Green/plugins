f4df3fd0759915d92ff8311082c76e619783ccb1
mosS-Green <76946302+mosS-Green@users.noreply.github.com>
2025-02-18 00:34:00 +0530

    Update telegraph.py

--- telegraph.py
+++ telegraph.py
@@ -1,9 +1,8 @@
 from pyrogram.enums import ParseMode
-
 from ub_core.utils.helpers import TELEGRAPH, post_to_telegraph
 
 from app import BOT, Message, bot
-from .text import text_gen, get_response_text, FAST, MEDIUM, SLOW, run_basic_check, get_slow_text
+from .aicore import ask_ai, DEFAULT, THINK, QUICK, run_basic_check
 
 
 @bot.add_cmd(cmd="rg")
@@ -25,34 +24,30 @@ async def generate_article(bot: BOT, message: Message):
         "IMPORTANT - Do not write inside an html code block."
         "IMPORTANT - Do not any give pretext. Immediately start with article."
     )
-
-    if "-t" in message.flags:
-        model = SLOW
-        get = get_slow_text
-    else:
-        model = MEDIUM
-        get = get_response_text
     
-    response = await text_gen(contents=base_prompt, **model)
-    article_content = get(response).strip('"')
+    article_content = await ask_ai(prompt=base_prompt, **DEFAULT)
 
     title_prompt = f"Generate a very concise and short title for this article: {article_content}. Only reply with the Title."
-    title_response = await text_gen(contents=title_prompt, **FAST)
-    title = get_slow_text(title_response)
+    title = await ask_ai(prompt=title_prompt, **QUICK)
 
     page_url = await post_to_telegraph(title, article_content)
 
-    await load_msg.edit(f"[{title}]({page_url})", parse_mode=ParseMode.MARKDOWN, disable_preview=True)
+    await load_msg.edit(f"**>\n**[{title}]({page_url})**<**", parse_mode=ParseMode.MARKDOWN, disable_preview=True)
 
 
 @bot.add_cmd(cmd="tf")
 @run_basic_check
 async def tf(bot: BOT, message: Message):
-    content = message.replied.text
-    title = message.input or "Telegraphed"
-
+    reply = message.replied
     load_msg = await message.reply("<code>...</code>")
+    
+    if reply and reply.text:
+        content = reply
+        title = message.input
+    else:
+        content = message.input
+        title = "Click to read"
 
     page_url = await post_to_telegraph(title, content)
 
-    await load_msg.edit(f"[{title}]({page_url})", parse_mode=ParseMode.MARKDOWN, disable_preview=True)
+    await load_msg.edit(f"**>\n**[{title}]({page_url})**<**", parse_mode=ParseMode.MARKDOWN, disable_preview=True)
