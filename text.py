91561fecf0ccd1297f01453e189e78394e965bc2
mosS-Green <76946302+mosS-Green@users.noreply.github.com>
2025-02-19 17:54:12 +0530

    Update text.py

--- text.py
+++ text.py
@@ -28,6 +28,24 @@ async def r_question(bot: BOT, message: Message):
     )
 
 
+@bot.add_cmd(cmd="rt")
+@run_basic_check
+async def fix(bot: BOT, message: Message):
+    reply = message.replied
+    prompts = message.input
+
+    load_msg = await message.reply("<code>...</code>")
+
+    title = "Scroll of Truth"
+    article = await ask_ai(
+        prompt=prompts, query=reply, **MODEL["THINK"]
+    )
+
+    page_url = await post_to_telegraph(title, article)
+
+    await load_msg.edit(f"[{title}]({page_url})", parse_mode=ParseMode.MARKDOWN, disable_preview=True)
+
+
 @bot.add_cmd(cmd="f")
 @run_basic_check
 async def fix(bot: BOT, message: Message):
