3be8f91a254eec85c31893bd8ee2317877699a8b
mosS-Green <leafinferno5@gmail.com>
2025-03-05 01:13:53 +0530

    add lastfm command and improve response messages

--- inline.py
+++ inline.py
@@ -4,6 +4,23 @@ from ub_core import BOT, Message
 from app.plugins.misc.inline_bot_results import run_with_timeout_guard
 
 
+@BOT.add_cmd("rn")
+@run_with_timeout_guard
+async def ub_lastfm(bot: BOT, message: Message):
+    """
+    CMD: rn
+    INFO: moment of laziness
+    USAGE: .rn
+    """
+
+    result: BotResults = await bot.get_inline_bot_results(bot="reyakamibot")
+
+    if not result.results:
+        return None, None, "No results found."
+
+    return result.query_id, result.results[0].id, ""
+
+
 @BOT.add_cmd("dl")
 @run_with_timeout_guard
 async def rsdl(bot: BOT, message: Message):
