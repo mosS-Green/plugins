4e7330a95fae7b17205dd4f8404c66d331edeb57
mosS-Green <76946302+mosS-Green@users.noreply.github.com>
2026-01-13 01:04:01 +0530

    ,,

--- social_dl.py
+++ social_dl.py
@@ -12,10 +12,13 @@ async def rsdl(bot: BOT, message: Message):
     USAGE: .d link
     """
     link = message.input or (message.replied and message.replied.text)
+
     if not link:
-        await message.reply("link nahi diya?")
+        await message.reply("link tera baap chor gaya tha ya teri ma?")
         return
+
     reya = bot.bot.me.id
+    leaf = bot.user.me.id
 
     processing_msg = await message.reply("Processing...")
 
@@ -30,18 +33,14 @@ async def rsdl(bot: BOT, message: Message):
             chat_id=reya, query_id=result.query_id, result_id=result.results[0].id
         )
 
-        await processing_msg.edit("downloading...")
-
         async with bot.Convo(
-            chat_id=reya,
-            client=bot,
-            from_user=bot.user.me.id,
+            chat_id=leaf,
+            client=bot.bot,
+            from_user=leaf,
             timeout=45,
             filters=filters.regex(r"sauce", re.IGNORECASE),
         ) as c:
-            await processing_msg.edit("Just a sec...")
             media = await c.get_response()
-            await processing_msg.edit("uploading...")
             await media.copy(message.chat.id, caption="")
             await processing_msg.delete()
 
